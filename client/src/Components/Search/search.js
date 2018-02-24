43902d7ca77340b6130389ffe7bd5e1ab20287d4
brett.g.hays <brett.g.hays.coding@gmail.com>
2018-02-24 11:37:17 -0700

    created search component

--- client/src/Components/Search/search.js
+++ client/src/Components/Search/search.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
-import UserCard from '../UserCard/userCard';
+import Navbar from '../Navbar/navbar';
+
 
 export default class Search extends Component {
     render() {
         return (
-            <div className = 'parent'>
-                <UserCard />
+            <div className = 'search_parent'>
+                <Navbar />
+                <div className="search_parent_container">
+                    <div className="search_child_container">
+                        <div className="search_child_top">
+                            <select name="Search Param">
+                                <option value="First Name">First Name</option>
+                                <option value="Last Name">Last Name</option>
+                            </select>
+                            <input type="text"/>
+                            <button className="black_btn">Search</button>
+                            <button className="gray_btn">Reset</button>
+                        </div>
+                        <div className="search_child_bottom"></div>
+                    </div>
+                </div>
+                
             </div>
         )
     }
